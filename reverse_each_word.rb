46a15e5beaa9334ef9c8c8b08d0bc1e26d37dea2
Melissa Sames <melissa.sames@gmail.com>
2018-01-15 00:16:14 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   sentence_as_array = string.split
   reversed_sentence = ''
 
-  sentence_as_array.each { |word|
+  sentence_as_array.collect { |word|
     reversed_sentence << word.reverse + ' '
   }
   reversed_sentence[0..-2]
