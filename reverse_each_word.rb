4f5f9692263efb46c41a7f8d23f149801f1fd7c2
Melissa Sames <melissa.sames@gmail.com>
2018-01-15 00:14:43 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+  sentence_as_array = string.split
+  reversed_sentence = ''
+
+  sentence_as_array.each { |word|
+    reversed_sentence << word.reverse + ' '
+  }
+  reversed_sentence
+end
